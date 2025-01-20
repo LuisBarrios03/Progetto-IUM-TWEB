ed04cd9df7c7117d65f0ca051e2194d77c05dadb
IreneRosa <irenerosa1812@gmail.com>
2025-01-20 17:12:04 +0100

    add actors
    Co-Authored-By: Luis Andrè Barrios Luna Victoria <91853345+LuisBarrios03@users.noreply.github.com>
    Co-Authored-By: Moumane Israa <166826618+iursraa@users.noreply.github.com>

---
+++ solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/actors/ActorsController.java
@@ -0,0 +1,13 @@
+package com.example.javaserver.actors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+
+public class ActorsController {
+}
---
+++ solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/actors/ActorsRepository.java
@@ -0,0 +1,13 @@
+package com.example.javaserver.actors;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+// Repositories
+@Repository
+public class ActorsRepository {
+}
