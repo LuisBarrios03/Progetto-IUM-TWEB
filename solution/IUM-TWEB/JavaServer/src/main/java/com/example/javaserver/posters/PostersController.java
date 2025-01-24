c4fc328641478f025b560ec77f5ceca00cd9ee31
Israa Moumane <israa.moumane@edu.unito.it>
2025-01-24 17:56:57 +0100

    very basic query for Posters

--- solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/posters/PostersController.java
+++ solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/posters/PostersController.java
@@ -1,4 +1,26 @@
 package com.example.javaserver.posters;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/posters")
 public class PostersController {
+    private final PostersRepository postersRepository;
+    @Autowired
+    public PostersController(PostersRepository postersRepository) {
+        this.postersRepository = postersRepository;
+    }
+    @GetMapping
+    public List<Posters> getAllPosters() {
+        return postersRepository.getPosters();
+    }
 }
--- solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/posters/PostersRepository.java
+++ solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/posters/PostersRepository.java
@@ -1,4 +1,17 @@
 package com.example.javaserver.posters;
 
+
+import com.example.javaserver.actors.Actors;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+@Repository
 public interface PostersRepository {
+    //getAll posters
+    @Query("select p from Posters p") List<Posters> getPosters();
 }
