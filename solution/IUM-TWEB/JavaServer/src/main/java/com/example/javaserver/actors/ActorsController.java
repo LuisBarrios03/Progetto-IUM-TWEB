290825641398976fe6afdb7a5a324da4c761db51
IreneRosa <irenerosa1812@gmail.com>
2025-01-26 17:28:31 +0100

    fixed actors

--- solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/actors/ActorsController.java
+++ solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/actors/ActorsController.java
@@ -1,4 +1,22 @@
 package com.example.javaserver.actors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class ActorsController {
+
+    private final ActorsService actorsService;
+    // define an Autowired service in the constructor
+    @Autowired
+    public ActorsController(ActorsService actorsService) {
+        this.actorsService = actorsService;
+    }
+
+    @GetMapping("/{indentity}")
+    public Actors getActors(@PathVariable Long identity) {
+        return ActorsRepository.findByIdentity(identity);
+    }
+
+
 }
--- solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/actors/ActorsRepository.java
+++ solution/IUM-TWEB/JavaServer/src/main/java/com/example/javaserver/actors/ActorsRepository.java
@@ -1,4 +1,23 @@
 package com.example.javaserver.actors;
 
-public interface ActorsRepository {
-}
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+    // Repositories
+    @Repository
+    public interface ActorsRepository extends JpaRepository<Actors, Long> {
+        // Trova tutti gli attori con un determinato ruolo
+        @Query(" SELECT a.name" +" FROM Actors a"+" WHERE a.role = :role")
+        List<Actors> findActorsByRole(@Param("role") String role);
+
+        // Trova tutti gli attori il cui nome contiene una stringa specifica
+        @Query("SELECT a FROM Actors a WHERE a.name LIKE %:name%")
+        List<Actors> findActorsByNameContains(@Param("name") String name);
+
+    }
