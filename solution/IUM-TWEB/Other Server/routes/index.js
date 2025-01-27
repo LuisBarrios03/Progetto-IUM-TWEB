200f1925eb18a56c53a43700c2d2e5ae23b6f29f
Luis Andrè Barrios Luna Victoria <luisbarriosaraujo@outlook.it>
2025-01-27 16:37:22 +0100

    delete other server

--- solution/IUM-TWEB/Other Server/routes/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controllerData = require('../Controller/controllersData');
-/* Post home page. */
-router.get('/api/data',controllerData.getAllData);
-module.exports = router;
