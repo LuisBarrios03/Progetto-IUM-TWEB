dae3b8b2be32030e5c750f346d52121f1ee4f7c4
Luis Andrè Barrios Luna Victoria <luisbarriosaraujo@outlook.it>
2025-01-30 11:48:40 +0100

    change searchbar

---
+++ solution/IUM-TWEB/MainServer/public/Js/index.js
@@ -0,0 +1,4 @@
+function toggleFilters() {
+    var filters = document.getElementById("filters");
+    filters.style.display = (filters.style.display === "none" || filters.style.display === "") ? "block" : "none";
+}
