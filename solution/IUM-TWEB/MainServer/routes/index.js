8cd20441c4cec2bbcdf80231160f4e63d2a06e65
Israa Moumane <israa.moumane@edu.unito.it>
2025-01-31 11:50:12 +0100

    page format fixes
    removed gaps, log in still needs work

--- solution/IUM-TWEB/MainServer/routes/index.js
+++ solution/IUM-TWEB/MainServer/routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 const axios = require('axios');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  res.render('Pages/HomePage.hbs', {title: 'Awards Explorer'});
+  res.render('Pages/HomePage.hbs', {title: 'Awards Explorer', showSearch: true});
 
 
 });
 
 /* GET film. */
 router.get('/Films', function(req, res, next) {
-  res.render('Pages/Films.hbs', { title: 'Films' });
+  res.render('Pages/Films.hbs', { title: 'Films', showSearch: true });
 });
 
 router.get('/Oscars', function(req, res, next) {
-  res.render('Pages/Oscars.hbs', { title: 'Oscars' });
+  res.render('Pages/Oscars.hbs', { title: 'Oscars', showSearch: true });
 });
 
 router.get('/Login', function(req, res, next) {
-  res.render('Pages/Login.hbs', { title: 'Login' });
+  res.render('Pages/Login.hbs', { title: 'Login', showSearch: false });
 });
 
 module.exports = router;
