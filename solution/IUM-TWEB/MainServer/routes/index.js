32c61f1dc75f52c8d028032f2577cb274797776d
IreneRosa <irenerosa1812@gmail.com>
2025-01-31 14:32:47 +0100

    add film scheda, attori e socket.io

--- solution/IUM-TWEB/MainServer/routes/index.js
+++ solution/IUM-TWEB/MainServer/routes/index.js
@@ -13,5 +13,10 @@ router.get('/Film', function(req, res, next) {
 router.get('/Oscars', function(req, res, next) {
   res.render('Pages/Oscars.hbs', { title: 'Express' });
 });
-
+router.get('/FilmScheda', function(req, res, next) {
+  res.render('Pages/FilmScheda.hbs', { title: 'Express' });
+});
+router.get('/Attori', function(req, res, next) {
+  res.render('Pages/Attori.hbs', { title: 'Express' });
+});
 module.exports = router;
