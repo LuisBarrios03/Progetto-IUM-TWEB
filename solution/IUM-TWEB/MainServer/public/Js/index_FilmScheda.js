8add59d4b41f6a611fa33f63ec883519997487c7
Luis Andrè Barrios Luna Victoria <luisbarriosaraujo@outlook.it>
2025-02-01 18:33:43 +0100

    update ui FilmScheda

--- solution/IUM-TWEB/MainServer/public/Js/index_FilmScheda.js
+++ solution/IUM-TWEB/MainServer/public/Js/index_FilmScheda.js
@@ -13,6 +13,9 @@ async function fetchFilmData() {
         document.getElementById('filmPoster').src = film.posters.link;
         document.getElementById('filmDuration').innerText = film.minute + " minuti";
         document.getElementById('filmRating').innerText = film.rating.toFixed(2);
+        let rating = film.rating !== null ? film.rating : 0;
+        let stars = generateStars(rating);
+        document.getElementById("startRating").innerHTML = stars;
         document.getElementById('filmDescription').innerText = film.description;
 
         const genres = film.genre.map(genre => `${genre.genre}`).join(', ');
@@ -32,6 +35,29 @@ async function fetchFilmData() {
     } catch (error) {
         console.error('Errore nel caricamento dei dati del film:', error);
     }
+}
+function generateStars(rating) {
+    let stars = "";
+    const fullStars = Math.floor(rating); // Numero di stelle piene
+    const halfStar = rating % 1 !== 0; // Verifica se c'è una mezza stella
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0); // Numero di stelle vuote
+
+    // Aggiungi stelle piene
+    for (let i = 0; i < fullStars; i++) {
+        stars += '<i class="fas fa-star text-warning"></i>';
+    }
+
+    // Aggiungi mezza stella (se necessario)
+    if (halfStar) {
+        stars += '<i class="fas fa-star-half-alt text-warning"></i>';
+    }
+
+    // Aggiungi stelle vuote
+    for (let i = 0; i < emptyStars; i++) {
+        stars += '<i class="far fa-star text-warning"></i>';
+    }
+
+    return stars;
 }
 
 function getMovieIdFromUrl() {
