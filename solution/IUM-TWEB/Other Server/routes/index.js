1de6a130b53870abfe7d08c09f6e19c69cf7c925
Israa Moumane <israa.moumane@edu.unito.it>
2025-02-02 22:28:57 +0100

    adjustements

--- solution/IUM-TWEB/Other Server/routes/index.js
+++ solution/IUM-TWEB/Other Server/routes/index.js
@@ -4,6 +4,5 @@ var controllerData = require('../Controller/controllersData');
 /* Post home page. */
 router.get('/api/dataRotten',controllerData.getAllDataRottenTomatoes);
 router.get('/api/dataOscar',controllerData.getAllDataTheOscarAwards);
-router.get('/api/20dataOscar',controllerData.get100OscarAwards);
 
 module.exports = router;
