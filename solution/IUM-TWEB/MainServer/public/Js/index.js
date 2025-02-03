57fb27062105eeec26b2849f4dc0787c2a8c5050
Luis Andrè Barrios Luna Victoria <luisbarriosaraujo@outlook.it>
2025-02-03 11:53:01 +0100

    update login for socket.io
    Co-Authored-By: IreneRosa <195474106+IreneRosa@users.noreply.github.com>
    Co-Authored-By: Moumane Israa <166826618+iursraa@users.noreply.github.com>

---
+++ solution/IUM-TWEB/MainServer/public/Js/index.js
@@ -0,0 +1,23 @@
+function initNavBar() {
+    let username = localStorage.getItem("username");
+    let userSection = document.getElementById("userSection");
+
+    if (username) {
+        // Se l'utente ha fatto il login, mostra il nome utente e il pulsante logout
+        userSection.innerHTML = `
+            <div class="dropdown">
+                <button class="btn btn-light dropdown-toggle me-5" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    ${username}
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                    <li><a class="dropdown-item" href="#" onclick="logout()">Logout</a></li>
+                </ul>
+            </div>
+        `;
+    }
+}
+
+function logout() {
+    localStorage.removeItem("username");
+    window.location.href = "/"; // Reindirizza alla home dopo il logout
+}
